0acd0d746ec8b9bd06745cc70a1573217b1c5db3
Ax3lMejia <axelmejia2851@gmail.com>
2025-04-27 15:30:47 -0400

    added to intro

--- intro.java
+++ intro.java
@@ -1,5 +1,10 @@
 class intro {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        for(int i=1; i<4; i++){
+            System.out.println("I love you this much "+ i);
+        }
     }
+
 }
